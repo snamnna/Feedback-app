7d7fd591027a90d217aedff6df62dafc97e53c87
tuiskuna <tuiskunarhi@gmail.com>
2023-08-29 11:18:44 +0300

    Tehty Login-sivun pohjaa ja siirretty kaikki frontin hommat Client-kansioon

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world! Tailwind toimii kai :DD
+        </h1>
+    )
+}
+
+export default App
---
+++ Client/src/Login.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import {useState} from "react";
+import swal from 'sweetalert';
+
+//Täs nyt mihin pääsin, apin kanssa pitää vähän säätää, tää tämän tutoriaalin pohjalta https://javascript.plainenglish.io/basic-react-login-using-external-api-e33322e480cd
+
+async function loginUser(credentials) {
+    return fetch('https://corsproxy.io/?https://www.mecallapi.com/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+
+
+const Login = () => {
+    const[username,setUsername]=useState('');
+    const[password, setPassword]=useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await loginUser({
+            username,
+            password
+        });
+        if ('accessToken' in response) {
+            swal("Success", response.message, "success", {
+                buttons: false,
+                timer: 2000,
+            })
+                .then((value) => {
+                    localStorage.setItem('accessToken', response['accessToken']);
+                    localStorage.setItem('user', JSON.stringify(response['user']));
+                    window.location.href = "/profile";
+                });
+        } else {
+            swal("Failed", response.message, "error");
+        }
+    }
+
+    return (
+        <>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Sign in to your account
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                                Username
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    onChange={e =>setUsername(e.target.value)}
+                                    required
+                                    placeholder="your username"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Password
+                                </label>
+                                <div className="text-sm">
+                                </div>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    onChange={e=>setPassword(e.target.value)}
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    placeholder="Your password"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                                type="submit"
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login;
