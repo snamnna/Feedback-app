0c54921475c7fdc9a7828b143ab05fecba8061ff
siljamat <siljama@hotmail.fi>
2023-09-06 12:02:42 +0300

    userservice

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -1,22 +1,64 @@
+const { PrismaClient } = require('@prisma/client')
 
+const prisma = new PrismaClient()
 
+//haetaan tietokannasta löytyykö useria
 async function checkUserExists(username) {
-//todo: haetaan tietokannasta löytyykö useria
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username,
+        }
+    })
 }
 
+//luodaan tietokantaan uusi user
 async function createUser(username,password){
-    //TODO: luodaan tietokantaan uusi user
+    const user = await prisma.user.create({
+        data: {
+            username: username,
+            password: password,
+        }
+    })
 }
 
+// poistetaan käyttäjä tietokannasta
 async function deleteUser(username,password){
-    //TODO: poistetaan käyttäjä tietokannasta
+    const deleteUser = await prisma.user.delete({
+        where: {
+            username: username,
+            password: password
+        },
+    })
 }
+
+//vertaa salasanaa tietokantaan
 async function validatePassword(username, password){
-    //TODO: vertaa salasanaa tietokantaan
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username,
+            password: password
+        }
+    })
 }
+
+//muokataan käyttäjää
+async function editUser(username, password) {
+    const updateUser = await prisma.user.update({
+        where: {
+            username: username,
+            password: password
+        },
+        data: {
+            username: username,
+            password: password
+        }
+    })
+}
+
 module.exports = {
     checkUserExists,
     createUser,
     deleteUser,
-    validatePassword
+    validatePassword,
+    editUser
 };
