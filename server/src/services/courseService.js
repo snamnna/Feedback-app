24fd0dbdb29b0fa34f56aaab42f5ca0e20f1e8e7
siljamat <siljama@hotmail.fi>
2023-09-07 20:23:19 +0300

    courseservicee

---
+++ server/src/services/courseService.js
@@ -0,0 +1,53 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+//haetaan tietokannasta löytyykö kurssia
+async function checkCourseExists(id, owner) {
+    const course = await prisma.course.findUnique({
+        where: {
+            id: id,
+            owner: owner,
+        }
+    })
+}
+
+//luodaan tietokantaan uusi kurssi
+async function createCourse(owner){
+    const course = await prisma.course.create({
+        data: {
+            owner: owner
+        }
+    })
+}
+
+// poistetaan kurssi tietokannasta
+async function deleteCourse(id, owner){
+    const deleteCourse = await prisma.course.delete({
+        where: {
+            id: id,
+            owner: owner
+        },
+    })
+}
+
+//muokataan kurssia
+async function editCourse(id, owner) {
+    const updateCourse = await prisma.course.update({
+        where: {
+            id: id,
+            owner: owner
+        },
+        data: {
+            id: id,
+            owner: owner
+        }
+    })
+}
+
+module.exports = {
+    checkCourseExists,
+    createCourse,
+    deleteCourse,
+    editCourse
+};
