df70053f1c998a9c9fbbd24b9d38b296d46a43ae
sanna <sanna.lohkovuori@hotmail.com>
2023-09-07 15:03:39 +0300

    Tehty app.js ja erotet. endpointit vaan controller

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,5 @@
-const express = require('express')
-const app = express()
+const app = require('./app');
 const port = 3001
-const authController = require('./controllers/authController')
-
-app.get('/', (req, res) => {
-
-})
-
-//Käytetään authcontrolleria reitin hallintaan
-app.use('/', authController)
-
-app.patch('/', (req, res) => {
-
-})
-
-app.delete('/', (req, res) => {
-    
-})
-
-
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
