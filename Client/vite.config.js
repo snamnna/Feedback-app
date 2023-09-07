4729d73da26d764baf321a4678d99fc3f02f33d3
Matias <matiasdn@metropolia.fi>
2023-09-07 13:15:30 +0300

    proxy /api pyynnöt 3001 porttiin

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+        '/api': {
+            target: 'http://localhost:3001',
+        }
+    }
+    }
 })
