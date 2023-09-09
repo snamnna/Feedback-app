ea9b75ceb97db3aad9bed22e9e3ad7146bd7ca13
Matias <matiasdn@metropolia.fi>
2023-09-09 17:03:07 +0300

    siirretty middlewaret app.js ja lisätty middleware joka logaa kaikki requestit consoleen

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const authController = require('./controllers/authController')
 const regController = require('./controllers/regController')
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}, body: ${JSON.stringify(req.body)}`)
+    next()
+})
 
 //Käytetään authcontrolleria reitin hallintaan
 app.use('/api/auth', authController)
