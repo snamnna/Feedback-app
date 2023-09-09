027c0a65bcd32c8e7264a5b2981cc055f9249117
Matias <matiasdn@metropolia.fi>
2023-09-09 17:00:43 +0300

    fixattu client port

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    port: 3000,
     open: true,
     proxy: {
         '/api': {
