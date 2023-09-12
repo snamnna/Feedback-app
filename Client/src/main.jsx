e4ffa7e0f5282babd2ca69cb9a71cf574d837145
Matias <matiasdn@metropolia.fi>
2023-09-12 15:02:58 +0300

    lisätty redux store ja provider

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from "./App.jsx";
-//import {AuthProvider} from "./AuthProvider.js";
+import { store } from './store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
           <App />
+    </Provider>
   </React.StrictMode>,
 )
