15606e68629e6546a8d22dcb693aee44b3592da4
Matias <matiasdn@metropolia.fi>
2023-09-12 16:35:23 +0300

    formatoitu prettierillä

--- Client/src/features/auth/authSlice.js
+++ Client/src/features/auth/authSlice.js
@@ -1,26 +1,26 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    token: null,
-    isAuthenticated: false,
-    user: null,
-}
+  token: null,
+  isAuthenticated: false,
+  user: null,
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        login(state, action) {
-            state.token = action.payload.token;
-            state.user = action.payload.user;
-            state.isAuthenticated = true;
-        },
-        logout(state) {
-            state.token = null;
-            state.user = null;
-            state.isAuthenticated = false;
-        },
+  name: "auth",
+  initialState,
+  reducers: {
+    login(state, action) {
+      state.token = action.payload.token;
+      state.user = action.payload.user;
+      state.isAuthenticated = true;
     },
+    logout(state) {
+      state.token = null;
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+  },
 });
 
 export const { login, logout } = authSlice.actions;
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App.jsx";
-import { store } from './store'
-import { Provider } from 'react-redux'
+import { store } from "./store";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-          <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
