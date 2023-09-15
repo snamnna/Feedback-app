2097aa8b8fa64e0cd5a0c946af4de77481ee964f
sanna <sanna.lohkovuori@hotmail.com>
2023-09-15 13:44:15 +0300

    Viimeistely vielä utils

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -4,3 +4,28 @@ const userService = require('../services/userService');
 const jwt = require('jsonwebtoken')
 
 //TODO: Tänne delete user / modify user ym
+
+router.get("/", checkToken, async (req, res) => {
+    // tän pitäis palauttaa kaikki käyttäjät databasesta
+})
+
+router.get("/:id", checkToken, async (req, res) => {
+    // tän pitäis palauttaa tietty käyttäjä urlissa parametrina olevan idn perusteella
+})
+
+router.put("/:id", checkToken, async (req, res) => {
+    // tällä pitäis pystyä päivittään tietyn käyttäjän tietoja. Pitää siis huolehtia, että käyttäjä voi päivittää vaan omia käyttäjätietojaan
+})
+
+router.delete("/:id", checkToken, async (req, res) => {
+    // tällä pitäis pystyä poistamaan käyttäjä (pitää taas huolehtii samast ku ylemmässä)
+})
+
+router.get("/:id/courses", checkToken, async (req, res) => {
+    // palauttaa kurssit, joissa käyttäjä on. Tarvitaan esim siihen ku näytetään frontin dashboardissa niitä kursseja.
+})
+
+router.get("/:id/feedback", checkToken, async (req, res) => {
+    // pitäis palauttaa kaikki käyttäjän antamat palautteet
+    // tähän tarvitaan myöhemmin kans toinen middleware, joka sit tarkastaa onko requestin lähettäny opettaja/admin tjsp ku ei haluta näyttää tuloksia kaikille
+})
--- server/src/utils/jwtUtils.js
+++ server/src/utils/jwtUtils.js
@@ -20,10 +20,10 @@ function tokenDecode(token){
     }
 }
 
-//Second function for token decoding
+//Second function for token decoding, which one is better?
 function tokenDecode2(token){
     const decoded = jwt_decode(token)
     return decoded
 }
 
-module.exports = { tokenSign, tokenVerify, tokenDecode } //Vaihda tähän se decode kumpi on oikea
+module.exports = { tokenSign, tokenDecode, tokenDecode2 } //Vaihda tähän se decode kumpi on oikea
