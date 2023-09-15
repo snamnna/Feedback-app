cf1b18e823f18531a3193dfe64bb55097d06c694
Matias <matiasdn@metropolia.fi>
2023-09-15 03:08:19 +0300

    implementoitu haku

--- Client/src/Pages/Dashboard/components/CoursesSection.jsx
+++ Client/src/Pages/Dashboard/components/CoursesSection.jsx
@@ -1,8 +1,43 @@
 import { FiPlus } from "react-icons/fi";
 import CourseCard from "./CourseCard";
 import PropTypes from "prop-types";
+import { useState } from "react";
+
+const placeholderCourses = [
+  {
+    name: "Course 1",
+    description: "Course description",
+    image: "https://picsum.photos/400/250",
+  },
+  {
+    name: "Course 2",
+    description: "Course description",
+    image: "https://picsum.photos/400/250",
+  },
+  {
+    name: "Course 3",
+    description: "Course description",
+    image: "https://picsum.photos/400/250",
+  },
+  {
+    name: "Course 4",
+    description: "Course description",
+    image: "https://picsum.photos/400/250",
+  },
+  {
+    name: "Course 5",
+    description: "Course description",
+    image: "https://picsum.photos/400/250",
+  },
+];
 
 export const CoursesSection = () => {
+  const [search, setSearch] = useState("");
+
+  const filterCourses = placeholderCourses.filter((course) =>
+    course.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="relative w-full overflow-hidden p-4">
       <div className="flex justify-between items-center pb-2">
@@ -22,9 +57,11 @@ export const CoursesSection = () => {
           className="p-2 border border-gray-300 rounded-md"
           type="text"
           placeholder="Search courses"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
       </div>
-      <CourseList />
+      <CourseList courses={filterCourses} />
     </div>
   );
 };
@@ -38,36 +75,6 @@ const CourseList = ({ courses }) => (
     </div>
   </div>
 );
-
-CourseList.defaultProps = {
-  courses: [
-    {
-      name: "Course 1",
-      description: "Course description",
-      image: "https://picsum.photos/400/250",
-    },
-    {
-      name: "Course 2",
-      description: "Course description",
-      image: "https://picsum.photos/400/250",
-    },
-    {
-      name: "Course 3",
-      description: "Course description",
-      image: "https://picsum.photos/400/250",
-    },
-    {
-      name: "Course 4",
-      description: "Course description",
-      image: "https://picsum.photos/400/250",
-    },
-    {
-      name: "Course 5",
-      description: "Course description",
-      image: "https://picsum.photos/400/250",
-    },
-  ],
-};
 
 CourseList.propTypes = {
   courses: PropTypes.arrayOf(
