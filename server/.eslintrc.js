801d0c2ae132e712603dcc1967944321979f2349
Matias <matiasdn@metropolia.fi>
2023-09-16 04:44:41 +0300

    lisätty eslint ja prettier

---
+++ server/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-console": "off",
+    "no-unused-vars": "warn",
+    "prettier/prettier": "error",
+  },
+};
