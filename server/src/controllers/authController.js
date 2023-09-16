e710eb11de652c33a8d3c6026f6b1ac1ee2608b0
Matias <matiasdn@metropolia.fi>
2023-09-16 03:04:27 +0300

    verifyToken siirretty middlewares.js

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -2,61 +2,30 @@ const express = require('express')
 const router = express.Router()
 const userService = require('../services/userService');
 const jwtUtils = require('../utils/jwtUtils')
-const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
-
-const secretKey = process.env.SECRET_KEY || 'oletusavain'
 
 //Authenticate User
 router.post("/", async (req, res) => {
+    const { username, password } = req.body;
 
-    try {
-        const { username, password } = req.body;
+    // Check if user exists in database
+    const user = await userService.getUserByUsername(username);
 
-        // Check if user exists in database
-        const user = await userService.checkUserExists(username);
+    if (!user) {
+        return res.status(401).json({message: "Invalid Credentials"});
+    }
 
-      if (!user) {
-        return res.status(401).json({ message: "Invalid Credentials" });
-      }
-
-      //Compare passwords
-      bcrypt.compare(password, user.password_hash, (err, result) => {
+    // Compare passwords
+    bcrypt.compare(password, user.password_hash, (err, result) => {
         if (result) {
-            //Sending token to frontend and message
             const token = jwtUtils.tokenSign(user)
-            return res.status(200).json({ message: "User Logged in Successfully", token });
+            return res.status(200).json({message: "User Logged in Successfully", token});
         }
-
         console.log(err);
-        return res.status(401).json({ message: "Invalid Credentials" });
-      });
-    } catch (error) {
-      res.status(401).send(error.message);
-    }
-  });
+        return res.status(401).json({message: "Invalid Credentials"});
+    });
+});
 
-
-// Funktion that is used in token auth (not used currently)
-function verifyToken(req, res, next) {
-    const bearerHeader = req.headers["authorization"];
-
-    if (typeof bearerHeader !== "undefined"){
-        const bearerToken = bearerHeader.split(" ")[1];
-        req.token = bearerToken;
-
-        jwt.verify(req.token, secretKey, (err, authData) => {
-            if (err) {
-                res.sendStatus(403);
-            } else {
-                req.authData = authData;
-                next();
-            }
-        });
-    } else {
-        res.sendStatus(403);
-    }
-}
 
 module.exports = router
 
