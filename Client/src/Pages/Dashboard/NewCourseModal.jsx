dc6f7d42b71c690a3f0283350b310c673039966e
matiasdn <matiasdn@metropolia.fi>
2023-09-16 17:48:37 +0300

    Viety kurssien luomisformi omaksi komponentiksi

--- Client/src/Pages/Dashboard/NewCourseModal.jsx
+++ Client/src/Pages/Dashboard/NewCourseModal.jsx
@@ -3,72 +3,7 @@ import { useState } from "react";
 import courseService from "../../services/courseService";
 import * as PropTypes from "prop-types";
 import { useSelector } from "react-redux";
-
-const CourseForm = ({
-  onSubmit,
-  courseName,
-  setCourseName,
-  courseDescription,
-  setCourseDescription,
-}) => {
-  if (courseDescription.length > 160)
-    setCourseDescription(courseDescription.slice(0, 160));
-
-  if (courseName.length > 64) setCourseName(courseName.slice(0, 64));
-
-  return (
-    <form className="form-control w-full max-w-lg" onSubmit={onSubmit}>
-      {/*  course name */}
-      <label className="label">
-        <span className="label-text">Course name</span>
-      </label>
-      <input
-        id={"course_name"}
-        type="text"
-        placeholder="name"
-        className="input w-full max-w-lg input-bordered"
-        value={courseName}
-        autoFocus={true}
-        onChange={(e) => setCourseName(e.target.value)}
-      />
-      <label className="label m-0">
-        <div></div>
-        <span className="label-text-alt">{`${
-          courseName.length || 0
-        } / 64`}</span>
-      </label>
-
-      {/*  course description */}
-      <label className="label">
-        <span className="label-text">Description</span>
-      </label>
-      <textarea
-        id={"course_description"}
-        className="textarea textarea-bordered max-h-24 resize-none"
-        placeholder="Description"
-        value={courseDescription}
-        onChange={(e) => setCourseDescription(e.target.value)}
-      ></textarea>
-      <label className="label mt-0">
-        <div></div>
-        <span className="label-text-alt">{`${
-          courseDescription.length || 0
-        } / 160`}</span>
-      </label>
-
-      {/*  Create button */}
-      <div className="flex justify-end pt-4">
-        <button
-          id={"create_course"}
-          className="btn btn-primary"
-          onClick={onSubmit}
-        >
-          Create
-        </button>
-      </div>
-    </form>
-  );
-};
+import { CourseForm } from "./components/CourseForm";
 
 CourseForm.propTypes = {
   onSubmit: PropTypes.func,
