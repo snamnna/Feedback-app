b3e4a9f63aa8c699388c5b09ef8db39fc74d7cf4
Matias <matiasdn@metropolia.fi>
2023-09-16 04:07:18 +0300

    muutettu käyttämään uutta tokenin vahvistus middlewarea

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,22 +2,26 @@ const express = require('express')
 const router = express.Router()
 const userService = require('../services/userService');
 const jwt = require('jsonwebtoken')
-const middlewares = require('../middlewares')
+const verifyToken = require('../middlewares/verifyToken')
 const courseService = require('../services/courseService')
 
 //TODO: Tänne delete user / modify user ym
 
-router.get("/", checkToken, async (req, res) => { 
+
+// TODO: Requestin lähettäjän id ja username on middlewaren avulla saatavilla req.user.id ja req.user.username muodossa. Tätä voi käyttää sitten vertailuun, että onko käyttäjä sama kuin se jonka tietoja yritetään muokata tai poistaa.
+// TODO: Ei tarvitse siis hakea databasesta käyttäjää id:n perusteella tai liittää bodyyn usernamea tai id:tä parametrin lisäksi
+
+router.get("/", verifyToken, async (req, res) => {
     // tän pitäis palauttaa kaikki käyttäjät databasesta ---- tarvitaan medodi joka palauttaisi kaikki, ei yksittäistä?
 })
 
-router.get("/:id", middlewares.checkToken, async (req, res) => {
+router.get("/:id", verifyToken, async (req, res) => {
     // tän pitäis palauttaa tietty käyttäjä urlissa parametrina olevan idn perusteella
 
     const userId = req.params.id;
 
     try{
-        const user = userService.checkUserExists(userId)
+        const user = userService.getUserById(userId)
 
         if(user) {
             res.json(user)
@@ -30,7 +34,7 @@ router.get("/:id", middlewares.checkToken, async (req, res) => {
     }
 })
 
-router.put("/:id", middlewares.checkToken, async (req, res) => {
+router.put("/:id", verifyToken, async (req, res) => {
     // tällä pitäis pystyä päivittään tietyn käyttäjän tietoja. Pitää siis huolehtia, että käyttäjä voi päivittää vaan omia käyttäjätietojaan
 
     const userId = req.params.id
@@ -39,7 +43,7 @@ router.put("/:id", middlewares.checkToken, async (req, res) => {
 
     try {
 
-        const user = await userService.checkUserExists(userId)
+        const user = await userService.getUserById(userId)
 
         if( !user ){
             return res.status(404).json({ error: "User can not be found"})
@@ -53,7 +57,7 @@ router.put("/:id", middlewares.checkToken, async (req, res) => {
             return res.status(403).json({ error: "Please enter username and password"})
         }
 
-        if (user.id == req.user.id) {
+        if (user.id === req.user.id) {
             const updated = await userService.editUser(username, password)
             res.json(updated)
         }
@@ -64,13 +68,13 @@ router.put("/:id", middlewares.checkToken, async (req, res) => {
 
 })
 
-router.delete("/:id", checkToken, async (req, res) => {
+router.delete("/:id", verifyToken, async (req, res) => {
     // tällä pitäis pystyä poistamaan käyttäjä (pitää taas huolehtii samast ku ylemmässä)
 
     const userId = req.params.id
 
     try{
-        const user = await userService.checkUserExists(userId)
+        const user = await userService.getUserById(userId)
 
         if( !user ){
             return res.status(404).json({ error: "User can not be found"})
@@ -84,7 +88,7 @@ router.delete("/:id", checkToken, async (req, res) => {
             return res.status(400).json({ eroor: "Username and password required"})
         }
 
-        if ( user.id == req.user.id ){
+        if ( user.id === req.user.id ){
             const updated = await userService.deleteUser(username, password)
             res.json(updated)
         }
@@ -94,7 +98,7 @@ router.delete("/:id", checkToken, async (req, res) => {
     }
 })
 
-router.get("/:id/courses", checkToken, async (req, res) => {
+router.get("/:id/courses", verifyToken, async (req, res) => {
     // palauttaa kurssit, joissa käyttäjä on. Tarvitaan esim siihen ku näytetään frontin dashboardissa niitä kursseja.
 
     const userId = req.params.id
@@ -114,7 +118,7 @@ router.get("/:id/courses", checkToken, async (req, res) => {
     }
 })
 
-router.get("/:id/feedback", checkToken, async (req, res) => {
+router.get("/:id/feedback", verifyToken, async (req, res) => {
     // pitäis palauttaa kaikki käyttäjän antamat palautteet
     // tähän tarvitaan myöhemmin kans toinen middleware, joka sit tarkastaa onko requestin lähettäny opettaja/admin tjsp ku ei haluta näyttää tuloksia kaikille
 })
