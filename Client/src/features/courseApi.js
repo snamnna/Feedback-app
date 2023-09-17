d7e78413cb9e9e9ca23232a35542aebcc31044e8
Matias <matiasdn@metropolia.fi>
2023-09-17 16:53:58 +0300

    courseApi

---
+++ Client/src/features/courseApi.js
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const courseApi = createApi({
+  reducerPath: "courseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/api/courses",
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    createCourse: builder.mutation({
+      query: ({ course, token }) => ({
+        url: "/",
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: course,
+      }),
+    }),
+    getCourseById: builder.query({
+      query: (courseId) => `${courseId}`,
+    }),
+  }),
+});
+
+export const { useGetCourseByIdQuery } = courseApi;
