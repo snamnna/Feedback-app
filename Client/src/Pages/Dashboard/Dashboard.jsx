e11d5e96a7db08d6880a6f32bc5313bfc2d31f11
Matias <matiasdn@metropolia.fi>
2023-09-17 16:48:21 +0300

    poistettu turha useEffect

--- Client/src/Pages/Dashboard/Dashboard.jsx
+++ Client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,7 @@
 import { DashboardHeader } from "./components/DashboardHeader";
 import { CoursesSection } from "./components/CoursesSection";
-import { useEffect } from "react";
-import courseService from "../../services/courseService";
 
 const Dashboard = () => {
-  useEffect(() => {
-    const fetchCourses = async () => {
-      try {
-        const courses = await courseService.getCourses();
-        console.log("Courses:", courses);
-      } catch (err) {
-        console.log("Error:", err);
-      }
-    };
-    fetchCourses().then(() => console.log("Courses fetched"));
-  }, []);
-
   return (
     <div className={"relative w-full overflow-hidden"}>
       <DashboardHeader />
