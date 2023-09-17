b5df2f39e59517680c655ec518b40177ccf7ba96
Matias <matiasdn@metropolia.fi>
2023-09-17 16:53:38 +0300

    userApi

---
+++ Client/src/features/userApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/api",
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getUserCourses: builder.query({
+      query: (id) => `/users/${id}/courses`,
+    }),
+  }),
+});
+
+export const { useLoginUserMutation, useGetUserCoursesQuery } = userApi;
