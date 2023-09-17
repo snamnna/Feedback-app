0a7fdcce74995704522eaf6caadb9b4e06acc30c
Matias <matiasdn@metropolia.fi>
2023-09-17 16:54:36 +0300

    lisätty apit storeen

--- Client/src/store.js
+++ Client/src/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import authReducer from "./features/auth/authSlice";
 import courseReducer from "./features/courses/courseSlice";
+import { userApi } from "./features/userApi";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     courses: courseReducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware),
 });
+
+setupListeners(store.dispatch);
