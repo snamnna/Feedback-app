2e4b063f0aa949727c021fa58054a59bf38dd6b2
sanna <sanna.lohkovuori@hotmail.com>
2023-09-18 19:11:14 +0300

    Poistettu try/catch, käyttää CustomErroria

--- server/src/controllers/regController.js
+++ server/src/controllers/regController.js
@@ -3,33 +3,24 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const userService = require("../services/userService");
+const CustomError = require("../utils/CustomError");
 
-// todo: try-catchit pois ja käytä custom erroria ja error handleria
+//Register
 router.post("/", async (req, res) => {
-  try {
     const { username, password } = req.body;
-
     const userExists = await userService.getUserByUsername(username);
 
-    if (userExists) {
-      console.log("käyttäjänimi/käyttäjä löytyy jo");
-
-      return res.status(401).json({ message: "Email is already in use." });
-    }
+    if (userExists) throw new CustomError(401, "Email already in use")
 
     const saltRounds = 10;
 
-    // Hashing password
+    // Hashing password 
+    //Tarvitsisiko tähän kuitenkin try catch vielä?
     const hash = await bcrypt.hash(password, saltRounds);
+    await userService.createUser(username, hash);
 
-    const nuser = await userService.createUser(username, hash);
-
-    // res.json({ message: "User created successfully", user: nuser });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
-  // todo: lähetetään onnistunut response tässä, ei tarvii palauttaa mitään ku halutaan vaan ilmottaa et käyttäjä luotu ja voi kirjautua sisään
+  //Successfull message, no need to return
   return res.status(200).json({ message: "User created successfully" });
 });
+
 module.exports = router;
