18c309f98569e0d32e6151d6b74b80a326056b78
tuiskuna <tuiskunarhi@gmail.com>
2023-09-19 12:06:45 +0300

    added footer

---
+++ Client/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p>Metropolia OTP project Feedbackapp by group 3</p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
