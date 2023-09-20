34744faf817be9f02bd3388020dd8d7a89eacea9
tuiskuna <tuiskunarhi@gmail.com>
2023-09-20 14:46:17 +0300

    added newlecturemodal

--- Client/src/Components/Layout.jsx
+++ Client/src/Components/Layout.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import NewCourseModal from "../Pages/Dashboard/NewCourseModal";
 import Footer from "./Footer";
+import NewLectureModal from "../Pages/CourseDetails/NewLectureModal";
 
 const Layout = () => {
   return (
     <>
       <NewCourseModal />
+      <NewLectureModal />
       <Header />
       <Outlet />
       <Footer />
