f93823b755b02f8c7733bc31f1f53617b0953fba
tuiskuna <tuiskunarhi@gmail.com>
2023-09-20 14:42:10 +0300

    add lecture implemented partially

--- Client/src/Pages/CourseDetails/CourseDetails.jsx
+++ Client/src/Pages/CourseDetails/CourseDetails.jsx
@@ -6,82 +6,115 @@ import PropTypes from "prop-types";
 import LectureCard from "./components/LectureCard";
 import { useSelector } from "react-redux";
 import courseService from "../../services/courseService";
-
-const placeholderLectures = [
-  {
-    id: 1,
-    name: "lecture 1",
-    feedback: "bad",
-  },
-  {
-    id: 2,
-    name: "lecture 2",
-    feedback: "bad",
-  },
-  {
-    id: 3,
-    name: "lecture 3",
-    feedback: "bad",
-  },
-  {
-    id: 4,
-    name: "lecture 4",
-    feedback: "bad",
-  },
-  {
-    id: 5,
-    name: "lecture 5",
-    feedback: "bad",
-  },
-];
+import { FiPlus } from "react-icons/fi";
 
 const CourseDetails = () => {
   const { courseId } = useParams();
   const [course, setCourse] = useState({});
-  const { data, error, isLoading } = useGetCourseByIdQuery(courseId);
   const [search, setSearch] = useState("");
   const [enrollmentStatus, setEnrollmentStatus] = useState("APPROVED"); //VÄLIAIKASESTI APPROVED TÄÄLLÄ KUN EI VIELÄ TOIMI
   const token = useSelector((state) => state.auth.token);
+  const { data, error, isLoading } = useGetCourseByIdQuery(courseId);
+  const [lectures, setLectures] = useState([]);
+  const [courseEnrollment, setCourseEnrollment] = useState([]);
 
-  //TODO: hae kurssista luennot ja filtteröi ne / laita ne esille
+  const placeholderLectures = [
+    {
+      id: 1,
+      name: "lecture 1",
+      feedback: "bad",
+    },
+    {
+      id: 2,
+      name: "lecture 2",
+      feedback: "bad",
+    },
+    {
+      id: 3,
+      name: "lecture 3",
+      feedback: "bad",
+    },
+    {
+      id: 4,
+      name: "lecture 4",
+      feedback: "bad",
+    },
+    {
+      id: 5,
+      name: "lecture 5",
+      feedback: "bad",
+    },
+  ];
 
+  //get data needed
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+      const { course, enrollment, lectures } = data;
+      setCourse(course);
+      setLectures(lectures);
+      setCourseEnrollment(enrollment);
+
+      console.log("debug:", courseEnrollment);
+    }
+  }, [data]);
+
+  //yritetään tehä jotai ownerin selvitystä en tiiä miten sen sais kunnol :D
+  let isOwner = true;
+  //lmao mist saan ton useridn oon taas hukas xD
+  /*if(course.lecturerId === userId){
+    isOwner = true;
+  }
+*/
+  //Filter lectures
   const filterLectures = placeholderLectures.filter((lecture) =>
     lecture.name.toLowerCase().includes(search.toLowerCase())
   );
 
-  useEffect(() => {
-    if (data) {
-      console.log(data);
-      const { course, courseEnrollment } = data;
-      setCourse(course);
-
-      //Yritystä enrollmentstatuksen hakuun.Pitäskö pystyä hakee tokenilla vrt userId jos ei, mitenhän saan käyttöön userid
-      /*      const findEnrollmentStatus = courseEnrollment.find(
-        (enrollment) => enrollment.user.id === token
-      );
-      //jos kurssin ilmottautumisista löytyy käyttäjä lisätään status
-      if (findEnrollmentStatus) {
-        setEnrollmentStatus(enrollmentStatus.status);
-      }*/
-    }
-  }, [data]);
-
-  const handleEnroll = async (e) => {
-    e.preventDefault();
-    const enroll = await courseService.courseEnrollment(course.id, token);
-    console.log("enroll");
+  //enrolling to a course
+  const handleEnroll = async () => {
+    const enrolldata = {
+      EnrollmentStatus: "APPROVED",
+    };
+    const enroll = await courseService.courseEnrollment(
+      courseId,
+      enrolldata,
+      token
+    );
+    console.log(enroll);
     //TODO: joku viesti et on tehty pyyntö ja sit joku systeemi et ei voi pyytää montaa kertaa et pääsis kurssille
+    // ja pitäs varmaa navigoida johonki sit kans
   };
 
-  //jos Enrollmentstatus on approved näytetään kurssin luennot
+  const openModal = () => {
+    const lectureModal = document.getElementById("new_lecture_modal");
+    if (lectureModal) {
+      lectureModal.showModal();
+    } else {
+      console.error("Modal element not found");
+    }
+  };
+
+  //Show lectures if enrollmentStatus is approved
   if (enrollmentStatus === "APPROVED") {
     return (
-      <div className="bg-secondary p-5">
-        <div className="flex justify-between mx-20 ">
-          <h1 className="my-2 text-xl font-bold">
-            {" "}
-            Lectures for course {course.name}
-          </h1>
+      <div className="bg-secondary ">
+        <div className="flex justify-between px-10 py-3 bg-base-100 shadow-md opacity-80">
+          <div className="flex flex-row ">
+            <h1 className="my-2 text-xl font-bold">
+              Lectures for course {course.name}
+            </h1>
+            {/*If the user is the owner of the course add lectures option*/}
+            {isOwner ? (
+              <button
+                className="btn btn-xs mt-1 px-1.5 py-1 box-content text-sm ml-4"
+                onClick={() => openModal()}
+              >
+                <FiPlus size={20} />
+                <span>Add lecture</span>
+              </button>
+            ) : null}
+          </div>
           <input
             className="border border-gray-300 rounded-md"
             type="text"
@@ -95,24 +128,21 @@ const CourseDetails = () => {
     );
   }
   return (
-    <div className="mx-10 my-20">
-      <div className="text-center my-30 max-w-full max-h-xl bg-secondary-content rounded-md p-5 shadow-md">
-        <h1 className="my-2 text-3xl font-bold">Details for {course.name}</h1>
-        <p className="my-2 mx-5">
-          {" "}
-          tähän tulee tekstiä kun hommat toimii
-          {course.description}
-        </p>
-        <button onClick={() => handleEnroll()} className="btn btn-primary my-2">
-          Request to join the course
-        </button>
-      </div>
+    <div className="mx-10 flex flex-col items-center justify-center h-full ">
+      <p className="text-lg">You are not yet enrolled in course</p>
+      <h1 className=" text-2xl font-bold">{course.name}</h1>
+      <h2 className="text-md font-bold">Description:</h2>
+      <p className="italic">{course.description}</p>
+      <button onClick={() => handleEnroll()} className="btn btn-primary my-2">
+        Request to join the course
+      </button>
     </div>
   );
 };
 
 const LectureList = ({ lectures }) => {
   const navigate = useNavigate();
+  console.log(lectures);
   const handleClick = (lectureId) => {
     // redirect to lecture details page
     navigate(`/lectures/${lectureId}`);
@@ -123,7 +153,7 @@ const LectureList = ({ lectures }) => {
       <ul className="flex flex-col mt-3">
         {lectures.map((lecture) => (
           <li
-            className="mx-20"
+            className="mx-10"
             key={lecture.id}
             onClick={() => handleClick(lecture.id)}
           >
