7662ddc5c440fa9db3931038184b9653298e4ff5
tuiskuna <tuiskunarhi@gmail.com>
2023-09-22 10:04:15 +0300

    tyylittelyä

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center p-4 bg-secondary text-base-content">
       <aside>
         <p>Metropolia OTP project Feedbackapp by group 3</p>
       </aside>
