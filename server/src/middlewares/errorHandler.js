ebb9b02ed590227bebb71b089d33033db130dd9b
matiasdn <matiasdn@metropolia.fi>
2023-09-23 20:10:26 +0300

    tulostetaan error stack vaan errorin sijaan

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -1,12 +1,12 @@
 const CustomError = require("../utils/CustomError");
 
-const errorHandler = (err, req, res, next) => {
+const errorHandler = async (err, req, res, next) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json({ message: err.message });
   }
 
   // todo: replace with logger when implemented
-  console.error(err);
+  console.error(err.stack);
 
   return res.status(500).json({ message: "Internal Server Error" });
 };
