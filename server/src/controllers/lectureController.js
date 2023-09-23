d72677ca1eb9991d711db7986ce52886262a47aa
sanna <sanna.lohkovuori@hotmail.com>
2023-09-23 12:27:50 +0300

    Lisätty kommentteja

--- server/src/controllers/lectureController.js
+++ server/src/controllers/lectureController.js
@@ -5,3 +5,5 @@ const userService = require("../services/userService");
 const feedbackService = require("../services/feedbackService")
 const verifyToken = require("../middlewares/verifyToken");
 const CustomError = require("../utils/CustomError");
+
+//Tämä odottaa lectureserviceä vielä
