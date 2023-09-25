72e3083dc07696aa4a08f36bd9815eb067b04414
siljamat <siljama@hotmail.fi>
2023-09-25 20:10:51 +0300

    tyylipohjaa käyttäjäasetuksille

--- Client/src/Pages/UserDetails/UserDetails.jsx
+++ Client/src/Pages/UserDetails/UserDetails.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { FiEdit, FiPlus, FiX } from "react-icons/fi";
+import { useParams } from "react-router";
 
 const UserDetails = () => {
+  const { userId } = useParams();
+
   return (
-    <div className="user-details">
-      <h1>Profile</h1>
+    <div className="flex flex-col">
+      <div className="px-10 py-3 flex flex-col items-center bg-base-100 opacity-90">
+        <h1 className="text-2xl font-semibold mb-4">User settings</h1>
+        <input
+          className="border border-gray-300 shadow-md rounded-md mb-6"
+          type="text"
+          placeholder="New username"
+        />
+        <input
+          className="border border-gray-300 shadow-md rounded-md mb-6"
+          type="text"
+          placeholder="Old password"
+        />
+        <input
+          className="border border-gray-300 shadow-md rounded-md mb-6"
+          type="text"
+          placeholder="New password"
+        />
+        <input
+          className="border border-gray-300 shadow-md rounded-md mb-6"
+          type="text"
+          placeholder="Repeat a new password"
+        />
+        <button className="btn btn-xs btn-primary shadow-md mb-6">
+          <span>Edit User</span>
+        </button>
+        <button className="btn btn-xs btn-primary shadow-md mb-6">
+          <span>Delete account</span>
+        </button>
+      </div>
     </div>
   );
 };
