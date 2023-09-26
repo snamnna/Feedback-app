70af5a609ac654186e58432ed48f96c990859af9
sanna <sanna.lohkovuori@hotmail.com>
2023-09-26 14:53:18 +0300

    Tehty lecturecontroller, lisätty reitiksi app

--- server/src/controllers/lectureController.js
+++ server/src/controllers/lectureController.js
@@ -1,38 +1,49 @@
 const express = require("express");
 
 const router = express.Router();
+const Joi = require("joi");
 const userService = require("../services/userService");
 const feedbackService = require("../services/feedbackService");
 const verifyToken = require("../middlewares/verifyToken");
 const CustomError = require("../utils/CustomError");
-const Joi = require(joi);
 const lectureService = require("../services/lectureService");
+const validate = require("../utils/validate");
 
 const lectureCreateSchema = Joi.object({
   lectureName: Joi.string().min(4).max(160).required(),
-  lectureId: Joi.number().integer().min(1).required(),
 });
 
-//Create lecture
-router.post("/lectures", verifyToken, async (req, res) => {
+// Create lecture
+router.post("/", verifyToken, async (req, res) => {
   if (req.user.userType !== "TEACHER") {
     return res.status(403).json({ message: "Permission denied" });
   }
 
   validate(lectureCreateSchema, req.body);
-  const { lectureName, lectureId } = req.body;
-  const id = parseInt(req.user.id, 10);
+  const { lectureName } = req.body;
+  const lectureId = parseInt(req.user.id, 10);
   const data = {
-    lectureName: lectureName,
-    lectureId: lectureId,
+    lectureName,
+    lectureId,
   };
 
   const newLecture = await lectureService.createLecture(data);
-  res.status(200).json(newLecture);
+  return res.status(200).json(newLecture);
 });
 
-//Get lecture feedback
+// Get lecture feedback
+router.get("/:id/feedback", verifyToken, async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const lecture = await lectureService.getLectureById(id);
 
-module.exports = {
-  lectureCreateSchema,
-};
+  if (!lecture) throw new CustomError(404, "Lecture not found");
+
+  const feedback = await lectureService.getAllFeedbacksOfLecture(id);
+  if (!feedback) throw new CustomError(404, "Feedback not found");
+
+  return res
+    .status(200)
+    .json({ message: "Feedback found succesfully", feedback });
+});
+
+module.exports = router;
