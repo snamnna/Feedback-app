3d39c1047ff1547d9942a12274e1f369cba17487
Matias <matiasdn@metropolia.fi>
2023-09-26 10:29:13 +0300

    fix config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
   rules: {
     "no-console": "off",
     "no-unused-vars": "warn",
-    "prettier/prettier": "warning",
+    "prettier/prettier": "warn",
     "jest/valid-expect": [
       "error",
       {
