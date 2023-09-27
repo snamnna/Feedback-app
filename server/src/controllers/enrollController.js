035aa84ebf741c87749ff702301c9ebec308020a
sanna <sanna.lohkovuori@hotmail.com>
2023-09-27 10:06:03 +0300

    Tehty pohja enrollControllerille

---
+++ server/src/controllers/enrollController.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+const Joi = require("joi");
+const courseService = require("../services/courseService");
+const feedbackService = require("../services/feedbackService");
+const verifyToken = require("../middlewares/verifyToken");
+const CustomError = require("../utils/CustomError");
+const validate = require("../utils/validate");
+
+module.exports = router;
