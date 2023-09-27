91bdae7d5392afa92ff74cbe709e648b6b39f14d
tuiskuna <tuiskunarhi@gmail.com>
2023-09-27 14:29:16 +0300

    tehty komponentti userin feedbackille

---
+++ Client/src/Pages/UserDetails/UserFeedbacks.jsx
@@ -0,0 +1,6 @@
+const UserFeedbacks = () => {
+  //TODO: hae kaikki userin antamat feedbackit ja laita ne tänne näytille
+  return <div>Tänne userin antamat feedbackit</div>;
+};
+
+export default UserFeedbacks;
