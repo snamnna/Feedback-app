e56ce64e85ec33c8728185cb546fc36e49536c0b
tuiskuna <tuiskunarhi@gmail.com>
2023-09-28 13:36:16 +0300

    lisätty feedbackmodal

--- Client/src/Components/Layout.jsx
+++ Client/src/Components/Layout.jsx
@@ -4,6 +4,7 @@ import NewCourseModal from "../Pages/Dashboard/NewCourseModal";
 import Footer from "./Footer";
 import NewLectureModal from "../Pages/CourseDetails/NewLectureModal";
 import EditCourseModal from "../Pages/Dashboard/components/EditCourseModal";
+import FeedbackModal from "../Pages/Feedback/FeedbackModal";
 
 const Layout = () => {
   return (
@@ -11,6 +12,7 @@ const Layout = () => {
       <NewCourseModal />
       <NewLectureModal />
       <EditCourseModal />
+      <FeedbackModal />
       <Header />
       <Outlet />
       <Footer />
