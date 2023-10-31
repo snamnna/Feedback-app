c5b9cb0951e77f374e5b18061e6e732ad71f632a
siljamat <siljama@hotmail.fi>
2023-10-31 19:57:22 +0200

    pohja lokaatiosaatiolle

---
+++ Client/src/i18n/config.js
@@ -0,0 +1,28 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import enTranslations from "./locales/en/translations.json";
+import faTranslations from "./locales/fa/translations.json";
+import fiTranslations from "./locales/fi/translations.json";
+
+i18n.use(initReactI18next).init({
+  fallbackLng: "en",
+  lng: "fi",
+  resources: {
+    en: {
+      translations: enTranslations,
+    },
+    fa: {
+      translations: faTranslations,
+    },
+    fi: {
+      translations: fiTranslations,
+    },
+  },
+  ns: ["translations"],
+  defaultNS: "translations",
+});
+
+i18n.languages = ["en", "fa", "fi"];
+
+export default i18n;
