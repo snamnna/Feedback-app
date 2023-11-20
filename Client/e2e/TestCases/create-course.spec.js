80e139f72a0fb2c3d8a079a47dcd751a8b0aeef2
sanna <sanna.lohkovuori@hotmail.com>
2023-11-20 14:49:29 +0200

    Tehty create course testi uusiksi

--- Client/e2e/TestCases/create-course.spec.js
+++ Client/e2e/TestCases/create-course.spec.js
@@ -2,31 +2,53 @@ import { test, expect } from "@playwright/test";
 
 const BASE_URL = "http://localhost:3000";
 
-test("Create course", async ({ page }) => {
-  await page.goto(`${BASE_URL}/login`);
+test.describe("create course", () => {
+  // Login before each test
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`${BASE_URL}/login`);
+    await page.getByLabel("Username").fill("teacher2");
+    await page.getByLabel("Password").fill("test");
+    await page.getByRole("button", { name: "login" }).click();
+    await page.waitForURL(`${BASE_URL}`);
+  });
 
-  // Wait for the username input field to be ready before filling it
-  await page.waitForSelector("input#username");
-  await page.fill("input#username", "teacher2");
+  // Testataan kurssin luominen
+  test("should create a new course and display it", async ({ page }) => {
+    // Open new course modal
+    await page.getByRole("button", { name: "New Course" }).click();
+    // Generate random course name and description
+    courseName = Math.random().toString(36).substring(7);
+    courseDescription = Math.random()
+      .toString(36)
+      .substring(7)
+      .repeat(6)
+      .substring(0, 32);
 
-  // Wait for the password input field to be ready before filling it
-  await page.waitForSelector("input#password");
-  await page.fill("input#password", "test");
+    // Fill course name and description
+    await page
+      .locator("#new_course_modal")
+      .getByPlaceholder("name")
+      .fill(courseName);
 
-  // Submit the form (assuming there's a submit button)
-  await page.click("button#submit-btn");
-  await page.waitForURL(`${BASE_URL}`);
+    await page
+      .locator("#new_course_modal")
+      .getByPlaceholder("Description")
+      .fill(courseDescription);
 
-  // Wait that course is created
-  await page.click('button:has-text("NEW COURSE")');
-  await page.fill("#courseName", "Kurssi123");
-  await page.fill("#description", "Kurssi jossa opetellaan laskemaan");
+    // Create course with pressing create button
+    await page
+      .locator("#new_course_modal")
+      .getByRole("button", { name: "Create" })
+      .click();
 
-  // Click create button
-  await page.click("button#submit-btn");
-  await page.waitForURL(`${BASE_URL}`);
+    // Check that course can be seen
+    expect(
+      page.getByText(`${courseName}${courseDescription}`).isVisible
+    ).toBeTruthy();
 
-  // Checks if the course is created
-  await page.goto(`${BASE_URL}`);
-  await page.waitForSelector(`a:has-text("Kurssi123")`);
+    // Check that course name and description are correctly showed
+    expect(
+      page.getByRole("heading", { name: courseName }).isVisible
+    ).toBeTruthy();
+  });
 });
