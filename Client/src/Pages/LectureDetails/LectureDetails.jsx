07ec0ee9bbd6b3d928d512f7e558cdb49771eba5
sanna <sanna.lohkovuori@hotmail.com>
2023-11-23 14:59:50 +0200

    lecturen feedbackien katsominen toimii

--- Client/src/Pages/LectureDetails/LectureDetails.jsx
+++ Client/src/Pages/LectureDetails/LectureDetails.jsx
@@ -133,7 +133,7 @@ const LectureDetails = () => {
               <h1 className="text-xl font-bold text-center">
                 List of feedbacks
               </h1>
-              <ul>
+              <ul id="lecturefeedbacklist">
                 {feedback.map((feedback, index) => (
                   <li
                     className="border rounded-md max-w-2xl p-3 my-5 mx-auto text-center"
